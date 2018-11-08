2b77154bcbd8062b3675f92bc16cb363186e8d3a
Russell <lihangen@lihangendeMacBook-Pro.local>
2018-11-08 20:45:52 +0800

    feat: change the file name

--- rename_file.py
+++ rename_file.py
@@ -1,7 +1,13 @@
 import os
 def rename_files():
-    #get file names from a folder
+    #(1)get file names from a folder
     file_list = os.listdir('./prank')
-    print(file_list)
+    #(2) for each file , rename filename
+    for file_name in file_list:
+        dele_number = file_name.translate(None,"0123456789")
+
+        os.rename(file_name,dele_number)
+
+
 
 rename_files()
